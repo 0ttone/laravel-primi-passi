e545ccb27a680965df140374625befd89c45790e
MyUsername <emanuele.marini37@gmail.com>
2021-09-01 17:27:32 +0200

    prove

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+</head>
+<body>
+      Sono la pagina 'about' generata in resources/view e con il percorso
+      generato all interno di  routes/web.php <br>
+      Questa è la chiave tra doppie graffe {{ $chiave }} che all interno di Routes è un array classico chiave-valore ma </br>
+      qui in view si traduce nel valore. 
+      Io sono la pagina {{ $pagina }}. L' /about/ che precede il punto è il valore della chiave pagina in routes.
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Lar8vel
                 </div>
 
                 <div class="links">
